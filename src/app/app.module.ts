011e0ded0b8b3679983d2706549beab277d9e564
Camilo Castrillon <jcastrillon@merqueo.com>
2022-07-03 20:48:13 -0500

    feat: i18n and components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { RowComponent } from './row/row.component';
+import { DialogComponent } from './dialog/dialog.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    RowComponent,
+    DialogComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
